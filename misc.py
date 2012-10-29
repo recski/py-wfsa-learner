fea8d3e6f1389ace8499da564736f432a670aa3d
ndavid <ndavid>
2012-10-29 15:08:02 +0000

    misc.py added for general purpose functions such as closureAndTopSort. Added Files: 	misc.py

---
+++ misc.py
@@ -0,0 +1,12 @@
+"""Misc functions."""
+
+# Closure: includes all prefixes of the strings.
+# Output topologically sorted according to the
+# partial ordering of "being a prefix". AKA sorted.
+def closureAndTopSort(strings) :
+    closed = set()
+    for string in strings :
+        for i in range(len(string)+1) :
+            closed.add(string[:i])
+    # logg( "Closure increased size from %d to %d." % (len(set(strings)),len(closed)) )
+    return sorted(list(closed))
