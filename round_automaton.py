76e8d4ccf36965ac830e807c55e5562e0492afb0
Attila Zseder <zseder@ilab.sztaki.hu>
2013-02-21 09:43:58 +0100

    modified:   round_automaton.py - lot of obsolete code removed, not done yet

--- round_automaton.py
+++ round_automaton.py
@@ -8,54 +8,6 @@ import logging
 import math
 import sys
 
-def my_log(x):
-    if x == 0:
-        return float('-inf')
-    else:
-        return math.log(x)
-
-def bit_round(x, n):
-    return round(x*((2**n)-1))/((2**n)-1)
-
-
-def round_32(weight, bit_no):
-    if weight in (0, 1): return weight
-    to_round = math.log(weight)/-32
-    rounded = bit_round(to_round, bit_no)
-    rounded_weight = math.exp(rounded*-32)
-    print weight, rounded_weight
-    return rounded_weight
-
-def round_transitions(transitions, bit_no, state1):
-    tr_by_weight = [(math.exp(log_weight), state)
-                    for (state, log_weight) in transitions.iteritems()]
-    tr_by_weight.sort()
-    tr_by_weight.reverse()
-    largest_prob, largest_prob_state = tr_by_weight[0]
-    rounded_transitions = dict([(state, round_32(weight, bit_no))
-                                for (weight, state) in tr_by_weight[1:]])
-    #print sum(rounded_transitions.values())
-    new_largest_prob = 1-sum(rounded_transitions.values())
-    #if new_largest_prob < 1:
-    #    print state1, largest_prob_state, largest_prob, new_largest_prob
-    #if largest_prob<0:
-    #    print largest_prob_state, foo, largest_prob
-    #    for state, weight in rounded_transitions.iteritems():
-    #        print state, weight
-    #    quit()
-    rounded_transitions[largest_prob_state] = new_largest_prob
-    rounded_transitions = dict([(state, my_log(weight))
-                                for state, weight in
-                                rounded_transitions.iteritems()])
-    return rounded_transitions
-
-def round_automaton(a, bit_no, file_name):
-    r_automaton = Automaton.create_from_dump(file_name)
-    for state1, transitions in a.m.iteritems():
-        rounded_transitions = round_transitions(transitions, bit_no, state1)
-        r_automaton.m[state1] = rounded_transitions
-    return r_automaton
-
 def get_weights(automaton, discard_ones_zeros=False):
     weights = {}
     for state1, transitions in automaton.m.iteritems():
@@ -67,9 +19,9 @@ def get_weights(automaton, discard_ones_zeros=False):
 
     return weights
 
-state_no = 3
-morpheme_no = 26
-word_no = 135
+#state_no = 3
+#morpheme_no = 26
+#word_no = 135
 
 def count_mdl(r_automaton, corpus, bit_no):
     kl_err = r_automaton.distance_from_corpus(
@@ -94,14 +46,6 @@ def count_error(automaton, r_automaton, file_name, bit_no):
         error = get_error(a_weights, r_a_weights, metric_name)
         print metric_name, error,
     print
-
-def main_old():
-    bit_no = int(sys.argv[2])
-    file_name = sys.argv[1]
-    automaton = Automaton.create_from_dump(file_name)
-    r_automaton = round_automaton(automaton, bit_no, file_name)
-    count_mdl(automaton, r_automaton, sys.stdin, file_name, bit_no)
-    #count_error(automaton, r_automaton, file_name, bit_no)
 
 def main():
     corpus = read_corpus(sys.stdin, '#')
