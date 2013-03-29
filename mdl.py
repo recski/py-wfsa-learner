e7ac18059cf74f2cf02ca4052e8cc3dd2f17bef2
Attila Zseder <zseder@ilab.sztaki.hu>
2013-03-29 15:03:24 +0100

    modified:   mdl.py -removed old code

--- mdl.py
+++ mdl.py
@@ -1,15 +1,5 @@
 import math
-import logging
 from automaton import Automaton
-
-def mdl(automaton, corpus, bits, distfp, n_state, n_alphabet, n_word):
-    logging.warning("Not implemented yet, mealy-moore problem.")
-    return 0
-    distance = automaton.distance_from_corpus(corpus, distfp)
-    err = distance * n_word
-    bits_per_transition = math.log(n_state ** 2, 2) + math.log(n_alphabet, 2)
-    result = transition_n * bits_per_transition + err
-    return result
 
 def moore_mdl(automaton, corpus, bits, n_words=135):
     dump = 0.0
