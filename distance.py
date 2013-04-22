5144b0c2e8dac08894723e31b310108cc4a21503
zseder <zseder@gmail.com>
2013-04-22 15:56:22 +0200

    new file:   distance.py for checking distances after learning, independent from anything

---
+++ distance.py
@@ -0,0 +1,20 @@
+import sys
+
+from automaton import Automaton
+from corpus import read_corpus, normalize_corpus
+
+def main():
+    # read automaton
+    wfsa = Automaton.create_from_dump(sys.argv[1])
+    # read corpus
+    corpus = read_corpus(open(sys.argv[2]), separator=sys.argv[3], skip=[sys.argv[4]])
+    normalize_corpus(corpus)
+    # call distance_from_corpus
+    distances = {}
+    dist = wfsa.distance_from_corpus(corpus, Automaton.kullback, distances=distances)
+    # print out result
+    for k, v in distances.iteritems():
+        print k, v
+
+if __name__ == "__main__":
+    main()
