682c28d55563e81e06a7e04c54eb766efa5f9f7b
Gabor Recski <recski@sztaki.hu>
2013-04-23 19:01:42 +0200

    function to run alphabet-type exeriments (one state for each letter) 	modified:   exp.py

--- exp/exp.py
+++ exp/exp.py
@@ -3,7 +3,7 @@ import os
 import logging
 
 sys.path.insert(0, sys.path[0].rsplit("/", 1)[0])
-from corpus import read_corpus, normalize_corpus
+from corpus import read_corpus, normalize_corpus, get_alphabet
 from create_baseline_automaton import create_word_wfsa
 from create_3state_wfsa import create_new_three_state_fsa
 from quantizer import LogLinQuantizer
@@ -120,6 +120,50 @@ class Exp(object):
 
         return [exp_name, bits_a, bits_e, bits_t, err]
 
+    def run_alphabet_exp(self, quantizer, distance, emissions):
+        exp_name = "{0}-{1}-{2}-{3}-{4}".format(
+            quantizer.bits,
+            abs(quantizer.neg_cutoff),
+            'm',
+            emissions,
+            distance[0])
+
+        logging.info("Running {0}".format(exp_name))
+
+        learnt_wfsa_filename = "{0}/{1}".format(self.workdir,
+            "learnt_{0}.wfsa".format(exp_name))
+
+        corpus = (self.morpheme_corpus if emissions == "m" else
+                  self.unigram_corpus)
+        # read Automaton or learn it and dump it finally
+        if os.path.exists(learnt_wfsa_filename):
+            # read already learnt automaton
+            learnt_wfsa = Automaton.create_from_dump(learnt_wfsa_filename)
+            learnt_wfsa.quantizer = quantizer
+            learnt_wfsa.round_and_normalize()
+        else:
+            # create and learn new automaton
+            alphabet = get_alphabet(corpus)
+            numbers_per_letters = dict([(letter, 1)
+                                        for letter in alphabet])
+            wfsa = Automaton.create_uniform_automaton(numbers_per_letters)
+            wfsa.finalize()
+            wfsa.quantizer = quantizer
+            wfsa.round_and_normalize()
+            learnt_wfsa = learn_wfsa(wfsa, corpus, distance)
+
+            # dump
+            with open(learnt_wfsa_filename, "w") as of:
+                learnt_wfsa.dump(of)
+
+        # encode automaton
+        encoder = (self.morpheme_encoder if emissions=="m" else
+                   self.unigram_encoder)
+        bits_a, bits_e, bits_t, err = encode_wfsa(
+            learnt_wfsa, corpus, encoder)
+
+        return [exp_name, bits_a, bits_e, bits_t, err]
+
 def run_exp(args):
     (exp, quantizer, distance, emission, type_) = args
 
@@ -127,4 +171,5 @@ def run_exp(args):
         return exp.run_list_exp(quantizer, emission)
     elif type_ in ["3", "a"] or type(type_) == list:
         return exp.run_learn_exp(quantizer, distance, type_, emission)
-
+    elif type_ == "m":
+        return exp.run_alphabet_exp(quantizer, distance, emission)
