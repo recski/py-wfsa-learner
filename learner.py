0b0906695bec7543e8b2c207d79c02a86a7a329d
zseder <zseder@gmail.com>
2013-06-12 16:06:02 +0200

    modified:   learner.py - now factors and temperatures are working in the new way

--- learner.py
+++ learner.py
@@ -10,7 +10,7 @@ from corpus import read_corpus, normalize_corpus
 
 class Learner(object):
     def __init__(self, automaton, corpus, pref_prob, distfp, turns_for_each,
-                factors):
+                factors, temperatures):
         """
         Learner class implements a simulated annealing method and
         is capable of applying downhill simplex as a preference instead
@@ -24,6 +24,9 @@ class Learner(object):
         @param factors: list of factors, length of list is how many bursts/
                         epochs we want to run, and every element is a float
                         which is the factor of the actual epoch
+        @param temperatures: same length list as factors and the elements are
+                             the tolerance parameters for the simmulated
+                             annealing
         """
         self.turns_for_each = turns_for_each
         self.preference_probability = pref_prob
@@ -33,7 +36,13 @@ class Learner(object):
 
         self.distfp = getattr(Automaton, distfp)
 
+        if len(temperatures) != len(factors):
+            print temperatures
+            print factors
+            raise Exception("temperatures has to have the same length as " +
+                           "factors when creating a Learner")
         self.factors = factors
+        self.temps = temperatures
         #self.preferred_node_pair = None
         #self.preferred_direction = None
         #self.disallowed_node_pair = None
@@ -42,8 +51,7 @@ class Learner(object):
     def create_from_options(automaton, corpus, options):
         return Learner(automaton, corpus, pref_prob=options.downhill_factor,
                        distfp=options.distfp, turns_for_each=options.iter,
-                       factor=options.factor, start_temp=options.start_temp,
-                       end_temp=options.end_temp, tempq=options.tempq)
+                       factors=options.factors, temperatures=options.temps)
 
     def change_automaton(self, options=None, revert=False):
         if not revert:
@@ -71,87 +79,71 @@ class Learner(object):
                     break
         change_options["edge"] = (s1, s2)
         change_options["factor"] = (factor if random.random() > 0.5
-                                    else -self.factor)
+                                    else -factor)
         return change_options
 
-    def choose_change_options(self, change_options_random):
+    def choose_change_options(self, change_options_random, *args):
         if random.random() < self.preference_probability:
             # downhill
             change_options = self.previous_change_options
         else:
-            change_options = change_options_random()
+            change_options = change_options_random(*args)
         return change_options
 
     def simulated_annealing(self, compute_energy, change_something,
                             change_back, option_randomizer):
-        temperature = self.start_temperature
-        end = self.end_temperature
-        tempq = self.temp_quotient
-        turns_for_each_temp = self.turns_for_each
+
         energy = compute_energy()
-        turn_count = 0
-        prev = None
-        prev_prev = None
-        for factor in self.factors:
-            if turn_count == 0:
-                logging.info("Running an iteration of Simulated Annealing with " +
-                             "{0} temperature and at {1} energy level.".format(
-                             temperature, energy))
+        for factor, temperature in zip(self.factors, self.temps):
+            for turn_count in xrange(self.turns_for_each):
+                if turn_count == 0:
+                    logging.info("Running an iteration of Simulated " + 
+                                 "Annealing with {0} factor ".format(factor) +
+                                 "and at {0} energy level.".format(energy))
 
-            change_options = self.choose_change_options(option_randomizer)
-            change_something(change_options)
-            new_energy = compute_energy()
-            energy_change = new_energy - energy
-            self.previous_change_options = change_options
-            if energy_change < 0:
-                accept = True
-            else:
-                still_accepting_probability = random.random()
-                accept = (still_accepting_probability < math.exp(-energy_change/temperature))
+                change_options = self.choose_change_options(option_randomizer,
+                                                            factor)
+                change_something(change_options)
+                new_energy = compute_energy()
+                energy_change = new_energy - energy
+                self.previous_change_options = change_options
+                if energy_change < 0:
+                    accept = True
+                else:
+                    still_accepting_probability = random.random()
+                    accept = (still_accepting_probability <
+                              math.exp(-energy_change/temperature))
 
-            if accept:
-                energy = new_energy
-                self.previous_change_options["result"] = True
-            else:
-                self.previous_change_options["result"] = False
-                change_back()
-
-            turn_count += 1
-            if turn_count == turns_for_each_temp:
-                turn_count = 0
-                temperature *= tempq
-                if temperature < end:
-                    break
-
-                #if prev_prev is not None and abs(energy - prev_prev) < 1e-8:
-                    #break
-                
-                prev_prev = prev
-                prev = energy
+                if accept:
+                    energy = new_energy
+                    self.previous_change_options["result"] = True
+                else:
+                    self.previous_change_options["result"] = False
+                    change_back()
 
     def main(self):
         compute_energy = lambda: self.automaton.distance_from_corpus(
                 self.corpus, self.distfp)
         change_something = lambda x: self.change_automaton(x, False)
         change_back = lambda: self.change_automaton(None, True)
-        option_randomizer = lambda: self.randomize_automaton_change()
+        option_randomizer = lambda x: self.randomize_automaton_change(x)
         self.simulated_annealing(compute_energy, change_something,
                                  change_back, option_randomizer)
 
+def csl2l_callback(option, opt, value, parser):
+    setattr(parser.values, option.dest, [float(_) for _ in value.split(',')])
+
 def optparser():
     parser = OptionParser()
-    parser.add_option("-f", "--factor", dest="factor", help="change factor " +
-                      "[default=%default]", default=0.97, type="float",
-                     metavar="FACTOR")
-    parser.add_option("-t", "--tempq", dest="tempq", default=0.9, type="float",
-                      help="temperature quotient [default=%default]",
-                      metavar="TEMPQ")
-    parser.add_option("", "--start_temp",dest="start_temp", default=1e-5,
-                      type="float", help="start temperature " + 
-                      "[default=%default]", metavar="TEMP")
-    parser.add_option("", "--end_temp", dest="end_temp", default=1e-7,
-                      type="float", help="end temperature " + 
-                      "[default=%default]", metavar="TEMP")
+    parser.add_option("-f", "--factors", dest="factors", help="comma " + 
+                      "separated list of change factors " + 
+                      "[default=%default]", default="0.2,0.4", 
+                      type="string", action="callback",
+                      callback=csl2l_callback)
+    parser.add_option("-t", "--temps", dest="temps", default="1e-5,1e-6",
+                      type="string", help="temperatures " + 
+                      "[default=%default]", action="callback",
+                      callback=csl2l_callback)
     parser.add_option("-i", "--iter",dest="iter", type="int", default=500,
                       help="number of iterations per temperature " +
                       "[default=%default]", metavar="I")
@@ -166,7 +158,6 @@ def optparser():
     parser.add_option("-o", "--output", dest="output", metavar="FILE",
                       type="str", default=None, help="File containing the " +
                       "dump of the learnt automaton [default=stdout]")
-
 
     parser.add_option("-a", "--automaton-file", dest="automaton_file",
                       metavar="FILE", type="str", default=None,
