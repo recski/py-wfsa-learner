f2037832fa3c330af608ee10b9f59964539e01f8
Gabor Recski <recski@sztaki.hu>
2013-06-12 12:12:42 +0200

    merged two nearly identical experiment functions (sze_tok and sze_type) 	modified:   exp.py

--- exp/exp.py
+++ exp/exp.py
@@ -39,9 +39,10 @@ def learn_wfsa(wfsa, corpus, distfp=None):
     wfsa_.round_and_normalize()
     if distfp is not None:
         learner = Learner(wfsa_, corpus, pref_prob=0.0,
-            distfp=distfp, turns_for_each=500, factor=0.8,
+            distfp=distfp, turns_for_each=50, factor=0.1,
             start_temp=1e-5, end_temp=1e-7, tempq=0.9)
         learner.main()
+    logging.debug("WFSA learnt")
     return wfsa_
 
 def encode_wfsa(wfsa, corpus, encoder):
@@ -126,7 +127,8 @@ class Exp(object):
 
         return [exp_name, bits_a, bits_e, bits_t, err, hq]
 
-    def run_sze_type_exp(self, quantizer, distance, emissions, state_bits):
+
+    def run_sze_exp(self, quantizer, distance, emissions, state_bits, entropy):
         exp_name = "{0}-{1}-{2}-{3}-{4}-{5}".format(
             quantizer.bits,
             abs(quantizer.neg_cutoff),
@@ -161,54 +163,17 @@ class Exp(object):
                 learnt_wfsa.dump(of)
 
         # encode automaton
-        encoder = Encoder(1.56655)
+        encoder = Encoder(entropy)
         bits_a, bits_e, bits_t, err, hq = encode_wfsa(
             learnt_wfsa, corpus, encoder)
-        return [exp_name, bits_a, bits_e, bits_t, err]
+        return [exp_name, bits_a, bits_e, bits_t, err, hq]
 
     def run_sze_tok_exp(self, quantizer, distance, emissions, state_bits):
-        exp_name = "{0}-{1}-{2}-{3}-{4}-{5}".format(
-            quantizer.bits,
-            abs(quantizer.neg_cutoff),
-            'm',
-            emissions,
-            distance[0], state_bits)
-
-        logging.info("Running {0}".format(exp_name))
-        learnt_wfsa_filename = "{0}/{1}".format(self.workdir,
-            "learnt_{0}.wfsa".format(exp_name))
-
-        corpus = self.unigram_corpus
-        # read Automaton or learn it and dump it finally
-        if os.path.exists(learnt_wfsa_filename):
-            # read already learnt automaton
-            learnt_wfsa = Automaton.create_from_dump(learnt_wfsa_filename)
-            learnt_wfsa.quantizer = quantizer
-            learnt_wfsa.round_and_normalize()
-        else:
-            # create and learn new automaton
-            alphabet = get_alphabet(corpus)
-            numbers_per_letters = dict([(letter, 1)
-                                        for letter in alphabet])
-            wfsa = Automaton.create_uniform_automaton(numbers_per_letters)
-            wfsa.finalize()
-            wfsa.quantizer = quantizer
-            wfsa.round_and_normalize()
-            learnt_wfsa = learn_wfsa(wfsa, corpus, distance)
-
-            # dump
-            with open(learnt_wfsa_filename, "w") as of:
-                learnt_wfsa.dump(of)
-
-<<<<<<< HEAD
-        # encode automaton
-        encoder = Encoder(0.933201)
-        bits_a, bits_e, bits_t, err = encode_wfsa(
-            learnt_wfsa, corpus, encoder)
-        return [exp_name, bits_a, bits_e, bits_t, err]
-=======
-        return [exp_name, bits_a, bits_e, bits_t, err, hq]
->>>>>>> 3358c8a389b7541a76a0f315ff7c590532ce607e
+        entropy = 0.933201
+        return self.run_sze_exp(quantizer, distance, emissions, state_bits, entropy)
+    def run_sze_type_exp(self, quantizer, distance, emissions, state_bits):
+        entropy = 1.56655
+        return self.run_sze_exp(quantizer, distance, emissions, state_bits, entropy)
 
 def run_exp(args):
     (exp, quantizer, distance, emission, type_, state_bits) = args
@@ -218,7 +183,7 @@ def run_exp(args):
     elif type_ in ["3", "a"] or type(type_) == list:
         return exp.run_3state_exp(quantizer, distance, type_, emission,
                                  state_bits)
-    elif type_ == "sze_tok":
+    elif type_ == "sze_toks":
         return exp.run_sze_tok_exp(quantizer, distance, emission, state_bits)
-    elif type_ == "sze_type":
+    elif type_ == "sze_types":
         return exp.run_sze_type_exp(quantizer, distance, emission, state_bits)
